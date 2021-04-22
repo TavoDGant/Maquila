ab24ed7d8dfcf804f48977bfd4f6cbcfd5cf4dd1
TavoDGant <61161002+TavoDGant@users.noreply.github.com>
2021-04-22 00:27:21 -0500

    CrossOrigin

--- src/main/java/com/tdgames/controller/EmpleadoController.java
+++ src/main/java/com/tdgames/controller/EmpleadoController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.tdgames.entity.Empleado;
 import com.tdgames.service.EmpleadoService;
 
+@CrossOrigin
 @RestController
 public class EmpleadoController {
 
